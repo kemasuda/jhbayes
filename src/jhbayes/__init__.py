d8c7517c0cce2ca3b31c0fdcdd7fbf7da88b08d9
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2022-06-01 14:08:41 +0900

    initial

---
+++ src/jhbayes/__init__.py
@@ -0,0 +1,11 @@
+__all__ = []
+
+__version__ = "0.0.1"
+__uri__ = "none"
+__author__ = "Kento Masuda"
+__email__ = ""
+__license__ = "MIT"
+__description__ = "hbayes with jax"
+
+#from . import hierarchical
+from .hierarchical import *
